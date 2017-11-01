5234564c438ee25fe76b20871dd9f43a8eb4026f
mterron <miguel.a.terron@gmail.com>
2017-11-01 22:55:16 +1300

    Updated example composition and orchestration

--- composition/start.sh
+++ composition/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # check for prereqs
-command -v docker >/dev/null 2>&1 || { printf "%s\n" "Docker is required, but does not appear to be installed."; exit; }
+command -v docker >/dev/null 2>&1 || { printf "%s\n" "Docker is required, but does not appear to be installed. See https://docs.joyent.com/public-cloud/api-access/docker"; exit; }
 
 # default values which can be overriden by -f or -p flags
 export COMPOSE_FILE=
@@ -21,7 +21,8 @@ export COMPOSE_HTTP_TIMEOUT=300
 
 printf "%s\n" 'Starting a Consul service'
 printf "%s\n" '>Pulling the most recent images'
-#docker-compose pull
+docker-compose pull
+
 # Set initial bootstrap host to localhost
 export CONSUL_BOOTSTRAP_HOST=127.0.0.1
 printf "%s\n" '>Starting initial container'
@@ -34,6 +35,12 @@ printf "%s\n" "CONSUL_BOOTSTRAP_HOST is $CONSUL_BOOTSTRAP_HOST"
 # Default for production
 BOOTSTRAP_UI_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONSUL_BOOTSTRAP_HOST")
 
+# For running on local docker-machine
+#if ! BOOTSTRAP_UI_IP=$(docker-machine ip); then {
+#	BOOTSTRAP_UI_IP=127.0.0.1
+#}
+#fi
+
 BOOTSTRAP_UI_IP="$CONSUL_BOOTSTRAP_HOST"
 
 printf "%s\n" " [DEBUG] BOOTSTRAP_UI_IP is $BOOTSTRAP_UI_IP"
@@ -41,16 +48,16 @@ BOOTSTRAP_UI_PORT=$(docker port "$CONSUL_BOOTSTRAP_HOST" | awk -F: '/8501/{print
 printf "%s\n" " [DEBUG] BOOTSTRAP_UI_PORT is $BOOTSTRAP_UI_PORT"
 
 export CONSUL_BOOTSTRAP_HOST=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONSUL_BOOTSTRAP_HOST")
-#CONSUL_BOOTSTRAP_HOST=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' "$CONSUL_BOOTSTRAP_HOST")
+#CONSUL_BOOTSTRAP_HOST=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' "$CONSUL_BOOTSTRAP_HOST")
 
 printf "%s\n" " [DEBUG] CONSUL_BOOTSTRAP_HOST is $CONSUL_BOOTSTRAP_HOST"
 
 # Wait for the bootstrap instance
 printf '>Waiting for the bootstrap instance...'
 TIMER=0
-until curl -fs --connect-timeout 1 http://"$BOOTSTRAP_UI_IP":"${BOOTSTRAP_UI_PORT-8500}"/ui &>/dev/null
+until curl -fs --connect-timeout 1 http://"$BOOTSTRAP_UI_IP":"${BOOTSTRAP_UI_PORT-8501}"/ui &>/dev/null 
 do
-	if [ $TIMER -eq 60 ]; then
+	if [ $TIMER -eq 30 ]; then
 		break
 	fi
 	printf '.'
@@ -67,3 +74,5 @@ command -v open >/dev/null 2>&1 && open https://"$BOOTSTRAP_UI_IP":"$BOOTSTRAP_U
 # Scale up the cluster
 printf "%s\n" 'Scaling the Consul raft to three nodes'
 docker-compose -p "$COMPOSE_PROJECT_NAME" scale consul=$CONSUL_CLUSTER_SIZE
+printf "%s\n" 'Remember to bootstrap the ACL system'
+
