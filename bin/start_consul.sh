74a7465c8b06f5ee5a1caab01e24a88c2e817181
mterron <miguel.a.terron@gmail.com>
2018-04-28 13:10:49 +1200

    Hard code Consul data directory to simplify startup

--- bin/start_consul.sh
+++ bin/start_consul.sh
@@ -12,8 +12,6 @@ set_consul_performance() {
 	PERFORMANCE=$1 su -s /bin/sh consul -c "{ rm /etc/consul/consul.json;jq '.performance.raft_multiplier=(env.PERFORMANCE|tonumber)' >/etc/consul/consul.json; } < /etc/consul/consul.json"
 }
 
-# Expand Consul data directory variable
-export CONSUL_DATA_DIR="${CONSUL_DATA_DIR:=/data}"
 
 # Add Consul FQDN to hosts file for convenience
 printf '%s\t%s\n' "$(hostname -i)" "$(hostname).node.${CONSUL_DNS:-consul}" >> /etc/hosts
@@ -74,7 +72,7 @@ if [ "$CONSUL_LOWEST_PORT" -le 1024 ]; then
 	fi
 fi
 
-if [ -e "$CONSUL_DATA_DIR/raft/raft.db" ]; then # This is a restart
+if [ -e "/data/raft/raft.db" ]; then # This is a restart
 # Bug in 1.0.6 fails if the key data_dir is not included
 #	consul validate -quiet /etc/consul/consul.json || exit 1
 	log 'Starting Consul'
@@ -82,14 +80,14 @@ if [ -e "$CONSUL_DATA_DIR/raft/raft.db" ]; then # This is a restart
 	unset CONSUL_BOOTSTRAP_HOST
 	unset CONSUL_CLUSTER_SIZE
 
-	exec su-exec consul:consul consul agent -server -ui -config-dir=/etc/consul/ -data-dir="$CONSUL_DATA_DIR" -datacenter="$CONSUL_DC_NAME" -domain="${CONSUL_DOMAIN:=consul}" -retry-join="$CONSUL_DNS_NAME" -rejoin
+	exec su-exec consul:consul consul agent -server -ui -config-dir=/etc/consul/ -data-dir=/data -datacenter="$CONSUL_DC_NAME" -domain="${CONSUL_DOMAIN:=consul}" -retry-join="$CONSUL_DNS_NAME" -rejoin
 
 else # This is the first start
 	if [ "$CONSUL_DC_NAME" ] && [ "$CONSUL_ENCRYPT_TOKEN" ] && [ "$CONSUL_CLUSTER_SIZE" ] && [ "$CONSUL_DNS_NAME" ]; then
 		log 'Starting Consul for the first time, using CONSUL_DC_NAME & BOOTSTRAP_HOST environment variables'
 		# Create Consul's data directory
-		mkdir -p -m 775 "$CONSUL_DATA_DIR"
-		chown -R consul: "$CONSUL_DATA_DIR"
+		mkdir -p -m 775 /data
+		chown -R consul: /data
 
 		# ACL Datacenter configuration
 		if [ -z "$CONSUL_ACL_DC" ]; then
@@ -97,9 +95,6 @@ else # This is the first start
 			export CONSUL_ACL_DC="$CONSUL_DC_NAME"
 		fi
 		su -s /bin/sh consul -c "{ rm /etc/consul/consul.json; jq '.acl_datacenter = env.CONSUL_ACL_DC' > /etc/consul/consul.json; } < /etc/consul/consul.json"
-
-		# Add ACL tokens to the config file (DEPRECATED)
-#		su -s /bin/sh consul -c "{ rm /etc/consul/consul.json; jq '.acl_agent_master_token = env.CONSUL_ACL_AGENT_MASTER_TOKEN | .acl_agent_token = env.CONSUL_ACL_AGENT_TOKEN | .acl_token = env.CONSUL_ACL_TOKEN' > /etc/consul/consul.json; } < /etc/consul/consul.json"
 
 	# Log Consul bootstrap host to the console
 		if [ "${CONSUL_BOOTSTRAP_HOST:-127.0.0.1}" = 127.0.0.1 ]; then
@@ -111,7 +106,7 @@ else # This is the first start
 
 # Bug in 1.0.6 fails if the key data_dir is not included
 #		consul validate /etc/consul/consul.json || exit 1
-		exec su-exec consul:consul consul agent -server -ui -config-dir=/etc/consul/ -data-dir="$CONSUL_DATA_DIR" -datacenter="$CONSUL_DC_NAME" -domain="${CONSUL_DOMAIN:=consul}" -bootstrap-expect="$CONSUL_CLUSTER_SIZE" -retry-join="${CONSUL_BOOTSTRAP_HOST:-127.0.0.1}" -retry-join="$CONSUL_DNS_NAME" -encrypt="$CONSUL_ENCRYPT_TOKEN"
+		exec su-exec consul:consul consul agent -server -ui -config-dir=/etc/consul/ -data-dir=/data -datacenter="$CONSUL_DC_NAME" -domain="${CONSUL_DOMAIN:=consul}" -bootstrap-expect="$CONSUL_CLUSTER_SIZE" -retry-join="${CONSUL_BOOTSTRAP_HOST:-127.0.0.1}" -retry-join="$CONSUL_DNS_NAME" -encrypt="$CONSUL_ENCRYPT_TOKEN"
 	else
 		printf 'Consul agent configuration\nUsage\n-----\n' >&2
 		printf 'You must always set the following environment variables to run this container:\nCONSUL_DC_NAME: The desired name for your Consul datacenter\n\n' >&2
