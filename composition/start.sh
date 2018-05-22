badbc646da9b2dcf8d22ebd6af098c8920d2f861
mterron <miguel.a.terron@gmail.com>
2018-05-22 15:24:41 +1200

    Simplified composition orchestration script

--- composition/start.sh
+++ composition/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
 # check for prereqs
-command -v docker >/dev/null 2>&1 || { printf "%s\n" "Docker is required, but does not appear to be installed. See https://docs.joyent.com/public-cloud/api-access/docker"; exit; }
+command -v docker >/dev/null 2>&1 || { printf "%s\n" "Docker is required, but does not appear to be installed."; exit; }
+test -e _env || { printf "%s\n" "_env file not found"; exit; }
+clear
 
 # default values which can be overriden by -f or -p flags
 export COMPOSE_FILE=
 export COMPOSE_PROJECT_NAME=demo
-export CONSUL_CLUSTER_SIZE=3
+export $(grep CONSUL_CLUSTER_SIZE _env)
 
 while getopts "f:p:" optchar; do
 	case "${optchar}" in
@@ -19,62 +20,64 @@ shift $(( OPTIND - 1 ))
 # give the docker remote api more time before timeout
 export COMPOSE_HTTP_TIMEOUT=300
 
-echo -e "Consul composition\e[m"
-
-printf "%s\n" 'Starting a Consul service'
-printf "%s\n" '>Pulling the most recent images'
-docker-compose pull
-# Set initial bootstrap host to localhost
-export CONSUL_BOOTSTRAP_HOST=127.0.0.1
-printf "%s\n" '>Starting initial container'
-docker-compose up -d --remove-orphans
-
+echo -e "Consul composition"
+printf "%s\n" "Starting a ${COMPOSE_PROJECT_NAME} Consul cluster"
+printf "\n* Pulling the most recent images\n"
+#docker-compose pull
+printf "\n* Starting initial container:\n"
+docker-compose up -d --remove-orphans --force-recreate
 
 CONSUL_BOOTSTRAP_HOST="${COMPOSE_PROJECT_NAME}_consul_1"
-printf "%s\n" "CONSUL_BOOTSTRAP_HOST is $CONSUL_BOOTSTRAP_HOST"
 
-# Default for production
-export BOOTSTRAP_UI_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONSUL_BOOTSTRAP_HOST")
-# For running on local docker-machine
-#if ! BOOTSTRAP_UI_IP=$(docker-machine ip); then {
-#	BOOTSTRAP_UI_IP=127.0.0.1
-#}
-#fi
-
-printf "%s\n" " [DEBUG] BOOTSTRAP_UI_IP is $BOOTSTRAP_UI_IP"
-#BOOTSTRAP_UI_PORT=$(docker port "$CONSUL_BOOTSTRAP_HOST" | awk -F: '/8501/{print$2}')
-BOOTSTRAP_UI_PORT=8501
-printf "%s\n" " [DEBUG] BOOTSTRAP_UI_PORT is $BOOTSTRAP_UI_PORT"
+BOOTSTRAP_UI_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONSUL_BOOTSTRAP_HOST")
+export CONSUL_BOOTSTRAP_HOST="$BOOTSTRAP_UI_IP"
 
 # Wait for the bootstrap instance
-# This fails in MACVLAN based environments as there's no connectivity between
-# host and containers. 30s should be enough to ensure the instance is up, but
-# should check for docker status too.
-printf '>Waiting for the bootstrap instance...'
+printf ' > Waiting for the bootstrap instance ...'
 TIMER=0
-until curl -fs --connect-timeout 1 http://"$BOOTSTRAP_UI_IP":"${BOOTSTRAP_UI_PORT:-8501}"/ui &>/dev/null
+until (docker-compose -p "$COMPOSE_PROJECT_NAME" exec consul su-exec consul: test -e /data/node-id)
 do
-	IS_RESTARTING=$(docker ps --quiet --filter 'status=restarting' --filter "name=${COMPOSE_PROJECT_NAME}_consul_1" | wc -l)
+	IS_RESTARTING=$(docker ps --quiet --filter 'status=restarting' --filter "name=${CONSUL_BOOTSTRAP_HOST}" | wc -l)
 	if [ "$IS_RESTARTING" -eq 1 ]; then
-		break
-	elif [ $TIMER -eq 30 ]; then
-		break
-	fi
-	printf '.'
-	sleep 1
-	TIMER=$(( TIMER + 1))
+		exit 1
+    elif [ $TIMER -gt 180 ]; then
+        exit 1
+    fi
+    printf '.'
+    sleep 3
+    TIMER=$(( TIMER + 3))
 done
+printf "\e[0;32m done\e[0m\n"
 
-sleep 5
-printf "%s\n" 'The bootstrap instance is now running'
-printf "%s\n" "Dashboard: https://$BOOTSTRAP_UI_IP:$BOOTSTRAP_UI_PORT/ui/"
-# Open browser pointing to the Consul UI
-command -v open >/dev/null 2>&1 && open https://"$BOOTSTRAP_UI_IP":"$BOOTSTRAP_UI_PORT"/ui/
 
 # Scale up the cluster
-printf "%s\n" 'Scaling the Consul raft to three nodes'
+printf "\n%s\n" "* Scaling the Consul raft to ${CONSUL_CLUSTER_SIZE} nodes"
 docker-compose -p "$COMPOSE_PROJECT_NAME" up -d --no-recreate --scale consul=$CONSUL_CLUSTER_SIZE
-printf "%s\n" 'Bootstrapping Consul ACL system'
-docker run -it --rm  -v $(pwd):/tmp --network=macvlan alpine:latest sh -c "apk --no-cache -q add curl jq && cd /tmp && echo \"BOOTSTRAP HOST IP IS: $BOOTSTRAP_UI_IP\" && echo \"$BOOTSTRAP_UI_IP consul.service.consul\" >> /etc/hosts &&echo -n -e \"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@ ACL Master Token is: \e[1;37m\$(curl -fs --cert client_certificate.pem --cert-type PEM --key client_certificate.key --cacert ca.pem -k -XPUT 'https://consul.service.consul:8501/v1/acl/bootstrap' | jq --compact-output --raw-output '.ID')\e[0m @\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\""
 
+# Wait for Consul to be available
+printf ' > Waiting for Consul cluster quorum acquisition and stabilisation ...'
+until (docker-compose -p "$COMPOSE_PROJECT_NAME" exec -w /tmp consul sh -c 'consul operator raft list-peers 2>/dev/null|grep -q leader')
+do
+	printf '.'
+	sleep 5
+done
+printf "\e[0;32m done\e[0m\n"
+
+
+printf "\n%s\n" '* Bootstrapping Consul ACL system'
+set -e
+
+CONSUL_TOKEN=$(docker-compose -p "$COMPOSE_PROJECT_NAME" exec -w /tmp consul sh -c "curl -s -XPUT http://127.0.0.1:8500/v1/acl/bootstrap 2>/dev/null | jq -M -e -c -r '.ID' | tr -d '\000-\037'")
+echo -e "Consul ACL token: \e[38;5;198m${CONSUL_TOKEN}\e[0m"
+
+# Install Agent token
+printf "%s" '> Installing Consul agent token ...'
+for ((i=1; i <= CONSUL_CLUSTER_SIZE ; i++)); do
+	docker-compose -p "$COMPOSE_PROJECT_NAME" exec -e CONSUL_TOKEN=$CONSUL_TOKEN -e AGENT_TOKEN=$CONSUL_TOKEN --index=$i -w /tmp consul sh -c 'curl -s --header "X-Consul-Token: $CONSUL_TOKEN" --data "{\"Token\": \"$CONSUL_TOKEN\"}" -XPUT http://127.0.0.1:8500/v1/agent/token/acl_agent_token'
+done
+printf "\e[0;32m done\e[0m\n"
+
+printf "%s\n\n" "Consul Dashboard: https://${BOOTSTRAP_UI_IP}:${BOOTSTRAP_UI_PORT:-8501}/ui/"
+# Open browser pointing to the Consul UI
+command -v open >/dev/null 2>&1 && open "https://$BOOTSTRAP_UI_IP:${BOOTSTRAP_UI_PORT:-8501}/ui/"
 
