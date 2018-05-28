06e382cd4e72ae0dbac5a67b4138f07964bcbba3
mterron <miguel.a.terron@gmail.com>
2018-05-29 01:57:34 +1200

    Correct quoting

--- composition/start.sh
+++ composition/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # check for prereqs
-command -v docker >/dev/null 2>&1 || { printf "%s\n" "Docker is required, but does not appear to be installed."; exit; }
-test -e _env || { printf "%s\n" "_env file not found"; exit; }
+command -v docker >/dev/null 2>&1 || { printf 'Docker is required, but does not appear to be installed.'; exit; }
+test -e _env || { printf '_env file not found'; exit; }
 clear
 
 # default values which can be overriden by -f or -p flags
@@ -20,16 +20,15 @@ shift $(( OPTIND - 1 ))
 # give the docker remote api more time before timeout
 export COMPOSE_HTTP_TIMEOUT=300
 
-echo -e "   ___                      _
+printf '   ___                      _
   / __\___  _ __  ___ _   _| |
- / /  / _ \| '_ \/ __| | | | |
+ / /  / _ \| ´_ \/ __| | | | |
 / /__| (_) | | | \__ \ |_| | |
-\____/\___/|_| |_|___/\__,_|_|"
-echo -e "Consul composition"
-printf "%s\n" "Starting a ${COMPOSE_PROJECT_NAME} Consul cluster"
-printf "\n* Pulling the most recent images\n"
-docker-compose pull
-printf "\n* Starting initial container:\n"
+\____/\___/|_| |_|___/\__,_|_|\n'
+printf 'Consul composition\n'
+printf '* Pulling the most recent images\n'
+#docker-compose pull
+printf '\n* Starting initial container:\n'
 docker-compose up -d --remove-orphans --force-recreate
 
 CONSUL_BOOTSTRAP_HOST="${COMPOSE_PROJECT_NAME}_consul_1"
@@ -56,11 +55,11 @@ do
     sleep 1
     TIMER=$(( TIMER + 1))
 done
-printf "\e[0;32m done\e[0m\n"
+printf '\e[0;32m done\e[0m\n'
 
 
 # Scale up the cluster
-printf "\n%s\n" "* Scaling the Consul raft to ${CONSUL_CLUSTER_SIZE} nodes"
+printf '\n%s\n' "* Scaling the Consul raft to ${CONSUL_CLUSTER_SIZE} nodes"
 docker-compose -p "$COMPOSE_PROJECT_NAME" up -d --no-recreate --scale consul=$CONSUL_CLUSTER_SIZE
 
 # Wait for Consul to be available
@@ -70,23 +69,23 @@ do
 	printf '.'
 	sleep 1
 done
-printf "\e[0;32m done\e[0m\n"
+printf '\e[0;32m done\e[0m\n'
 
 
-printf "\n%s\n" '* Bootstrapping Consul ACL system'
+printf '\n* Bootstrapping Consul ACL system\n'
 set -e
 
 CONSUL_TOKEN=$(docker-compose -p "$COMPOSE_PROJECT_NAME" exec -w /tmp consul sh -c "curl -s -XPUT http://127.0.0.1:8500/v1/acl/bootstrap 2>/dev/null | jq -M -e -c -r '.ID' | tr -d '\000-\037'")
-echo -e "Consul ACL token: \e[38;5;198m${CONSUL_TOKEN}\e[0m"
+printf "Consul ACL token: \e[38;5;198m${CONSUL_TOKEN}\e[0m\n"
 
 # Install Agent token
-printf "%s" ' > Installing Consul agent token ...'
+printf ' > Installing Consul agent token ...'
 for ((i=1; i <= CONSUL_CLUSTER_SIZE ; i++)); do
 	docker-compose -p "$COMPOSE_PROJECT_NAME" exec -e CONSUL_TOKEN=$CONSUL_TOKEN -e AGENT_TOKEN=$CONSUL_TOKEN --index=$i -w /tmp consul sh -c 'curl -s --header "X-Consul-Token: $CONSUL_TOKEN" --data "{\"Token\": \"$CONSUL_TOKEN\"}" -XPUT http://127.0.0.1:8500/v1/agent/token/acl_agent_token'
 done
-printf "\e[0;32m done\e[0m\n"
+printf '\e[0;32m done\e[0m\n'
 
-printf "%s\n\n" "Consul Dashboard: https://${BOOTSTRAP_UI_IP}:${BOOTSTRAP_UI_PORT:-8501}/ui/"
+printf '\n%s\n' "Consul Dashboard: https://${BOOTSTRAP_UI_IP}:${BOOTSTRAP_UI_PORT:-8501}/ui/"
 # Open browser pointing to the Consul UI
 command -v open >/dev/null 2>&1 && open "https://$BOOTSTRAP_UI_IP:${BOOTSTRAP_UI_PORT:-8501}/ui/"
 
